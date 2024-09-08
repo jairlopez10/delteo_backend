fb739930b5815407c7e7d527d419340f9bcf1837
Jair Lopez <lopezarias7@hotmail.com>
2024-09-08 12:05:51 -0500

    Agregar a orderdbdetailed los pedidos

--- helpers/enviarpedidoinfo.js
+++ helpers/enviarpedidoinfo.js
@@ -4,22 +4,35 @@ const enviarpedidoinfo = async (datos) => {
 
     const { cliente, origen, fecha, productostext, productos, ciudad, direccion, telefono, total, cedula } = datos
 
+
+    
+
     const auth = await google.auth.getClient({
         scopes: ['https://www.googleapis.com/auth/spreadsheets']
     })
 
     const sheets = google.sheets({version: 'v4', auth});
 
-    const rangenextrow = 'Settings!A2';
+    const rangenextrow = 'Settings!A2:B2';
 
     const responseget = await sheets.spreadsheets.values.get({
         spreadsheetId: process.env.SHEET_ID,
         range: rangenextrow,
     })
 
-    const nextrowtoprint = +responseget.data.values[0][0]
+    const nextrowtoprintorders = +responseget.data.values[0][0]
+    const nextrowtoprintdetaild = +responseget.data.values[0][1]
 
-    const range = `Orders!B${nextrowtoprint}`;
+    const range = `Orders!B${nextrowtoprintorders}`;
+    const rangedetaild = `Orders_DB_detailed!B${nextrowtoprintdetaild}`
+
+    let orderdetaildprint = [];
+
+    productos.forEach((prod) => {
+        orderdetaildprint.push([nextrowtoprintorders-1, prod.id, prod.cantidad])
+    })
+
+    console.log(datos);
 
     const response = await sheets.spreadsheets.values.append({
         spreadsheetId: process.env.SHEET_ID,
@@ -29,6 +42,17 @@ const enviarpedidoinfo = async (datos) => {
             values: [[cliente, origen, fecha, productostext, ciudad, direccion, telefono, cedula, total]]
         }
     })
+
+    const responsedetailed = await sheets.spreadsheets.values.append({
+        spreadsheetId: process.env.SHEET_ID_ORDERDETAILD,
+        range: rangedetaild,
+        valueInputOption: 'USER_ENTERED',
+        resource: {
+            values: orderdetaildprint
+        }
+    })
+
+
 
 }
 
